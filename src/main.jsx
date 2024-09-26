b8ff6c8abac9661716a6773a3dcf44b816192f12
seungmin2222 <wooju1701@hanmail.net>
2024-09-27 03:48:24 +0900

    💄 Design: 라우터 설정 및 Play 페이지 정적 구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
 import App from './App.jsx';
-import Play from './Play.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Play />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
