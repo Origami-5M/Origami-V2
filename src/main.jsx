25f991f2e838821b0efc81c1d3ac4c6184c359a5
seungmin2222 <wooju1701@hanmail.net>
2024-09-27 00:27:25 +0900

    💄 Design: Play 페이지 정적 화면 구현 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import Play from './Play.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Play />
   </StrictMode>
 );
