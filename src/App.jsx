11949dda2fd651f1616654bfe556ce2f05dd65ea
Jeongsohee <toy3045@gmail.com>
2024-09-28 01:50:24 +0900

    ✨ Feat: gallery 페이지 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Play from './Play.jsx';
 import MainPage from './MainPage.jsx';
+import Gallery from './GalleryPage.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/play" element={<Play />} />
+      <Route path="/gallery" element={<Gallery />} />
     </Routes>
   );
 };
